0a73b76800867486279b82c3dc11176180b7ae8d
Raghavendranath Kandula <raghavendranath.kandula525@gmail.com>
2017-12-06 01:46:07 -0600

    Added clean and stem function

--- src/similarity/cs/semantics/Main.java
+++ src/similarity/cs/semantics/Main.java
@@ -12,11 +12,17 @@ package similarity.cs.semantics;
 import org.apache.commons.cli.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import opennlp.tools.stemmer.*;
 
 public class Main {
+    //Stop words
+    static ArrayList<String> stopWords = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println("Hi Sam");
+    public static void main(String[] args) throws FileNotFoundException{
+        //System.out.println("Hi Sam");
         Options options = new Options();
         options.addRequiredOption("f", "file", true, "input file to process");
         options.addOption("h", false, "print this help message");
@@ -33,15 +39,69 @@ public class Main {
         }
 
         String filename = cmd.getOptionValue("f");
-		if (!new File(filename).exists()) {
-			System.err.println("file does not exist "+filename);
-			System.exit(1);
-		}
-
+	if (!new File(filename).exists()) {
+            System.err.println("file does not exist "+filename);
+            System.exit(1);
+	}
+        //Populating stop words in ArrayList
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader("C:\\Users\\ragha\\OneDrive\\Documents\\NetBeansProjects\\Similarity\\stopwords.txt"));
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                stopWords.add( sCurrentLine.trim());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(br!=null)
+                    br.close();
+                }
+            catch(IOException io){
+                io.printStackTrace();
+            }
+        }
+                
+        Scanner sc = new Scanner(new File(filename)).useDelimiter("(\\.|\\?|\\!)");
+        ArrayList<String[]> words = new ArrayList<>();
+        while(sc.hasNext()){
+            String[] temp = cleanUpAndStem(sc.next().trim());
+            words.add(temp);
+        }
+              
+        for(String[] word: words){
+            for(String str: word){
+                System.out.print(str+",");
+            }
+            System.out.println();
+        }
+                
         if (cmd.hasOption("h")) {
             HelpFormatter helpf = new HelpFormatter();
             helpf.printHelp("Main", options, true);
             System.exit(0);
         }
+        
+        System.out.println(filename);
+        
+        
+        
     }
+    
+    public static String[] cleanUpAndStem(String sent){
+        String arr[] = sent.toLowerCase().replaceAll("\n"," ").replaceAll(","," ").split(" ");
+        String unique[]= Arrays.stream(arr).distinct().toArray(String[]::new);
+        ArrayList<String> result = new ArrayList<>();
+        PorterStemmer ptr = new PorterStemmer();
+        for(String str: unique){
+            if(!stopWords.contains(str) && !str.isEmpty()){
+                String temp = ptr.stem(str);
+                result.add(temp);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+    
 }
+  
